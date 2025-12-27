2b31edc6ff047d182f7993cc79dc0ca495d23fc8
nanayuki <q2577358244@gmail.com>
2025-12-27 14:23:30 +0800

    init: first commit

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+struct Complex{
+    int r;
+    int i;
+};
+Complex Mul(Complex c1,Complex c2){
+    Complex result;
+    result.r=c1.r*c2.r+
+}
